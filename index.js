e7d959d65c0f62bed0a7552748a4052c231f3bc9
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-07-06 01:15:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 var currentUser = 'Me';
 
-const welcomeMessage = "Welcome to Flatbook, ${currentUser}!";
+var welcomeMessage = "Welcome to Flatbook, ${currentUser}!";
 
 var excitedWelcomeMessage =
